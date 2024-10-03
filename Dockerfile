c858175b1831b45a6a5abcd598890cd0b5f022f8
thansetan <thansetan@gmail.com>
2024-10-03 23:16:45 +0700

    pindah railway

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.23.2 AS builder
+
+WORKDIR /app
+COPY go.mod go.sum ./
+RUN go mod tidy
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -o ./berak .
+
+FROM alpine:latest
+
+WORKDIR /app
+COPY --from=builder /app/berak /app/example.env ./
+EXPOSE ${PORT}
+CMD "./twitter-moon"
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/thansetan/berak/berak"
+	"github.com/thansetan/berak/db"
+)
+
+func main() {
+	db, err := db.NewConn(os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		panic(err)
+	}
+	repo := berak.NewRepo(db)
+	tmpl := template.Must(template.New("").Funcs(template.FuncMap{
+		"add": func(a, b int) int {
+			return a + b
+		},
+		"getMonthName": func(monthNumber int) string {
+			if monthNumber == 0 || monthNumber > 12 {
+				return ""
+			}
+			return []string{
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December",
+			}[monthNumber-1]
+		},
+		"tai": func(n int) string {
+			return strings.Repeat("💩", n)
+		},
+	}).ParseGlob("templates/*.html"))
+	controller := berak.NewController(repo, tmpl)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now()
+		http.Redirect(w, r, fmt.Sprintf("/%d", now.Year()), http.StatusTemporaryRedirect)
+	})
+	mux.Handle("POST /berak", rateLimit(protected(controller.Create)))
+	mux.Handle("DELETE /berak", rateLimit(protected(controller.Delete)))
+	mux.HandleFunc("GET /{year}", controller.GetMonthly)
+	mux.HandleFunc("GET /{year}/{month}", controller.GetDaily)
+	err = http.ListenAndServe(":"+os.Getenv("PORT"), mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func protected(next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-API-KEY") != os.Getenv("BERAK_KEY") {
+			w.WriteHeader(http.StatusForbidden)
+			_, _ = w.Write([]byte("gaboleh"))
+			return
+		}
+		next(w, r)
+	}
+}
+
+var users = new(sync.Map)
+
+func rateLimit(next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-KEY")
+		if apiKey == "" {
+			w.WriteHeader(http.StatusForbidden)
+			_, _ = w.Write([]byte("gaboleh"))
+			return
+		}
+		lastAccess, ok := users.LoadOrStore(apiKey, time.Now())
+		if ok {
+			lastAccessedAt, ok := lastAccess.(time.Time)
+			if !ok {
+				w.WriteHeader(http.StatusInternalServerError)
+				_, _ = w.Write([]byte("error"))
+				return
+			}
+			if time.Since(lastAccessedAt) < time.Minute {
+				w.WriteHeader(http.StatusTooManyRequests)
+				_, _ = w.Write([]byte("kecepeten"))
+				return
+			}
+			users.Store(apiKey, time.Now())
+		}
+		next(w, r)
+	}
+}
