62d393eb0e35277b0b6b94990c6e0e4d0b883e8c
thansetan <thansetan@gmail.com>
2024-10-04 10:55:47 +0700

    feat: add logging

--- berak/controller.go
+++ berak/controller.go
@@ -1,38 +1,55 @@
 package berak
 
 import (
+	"encoding/json"
 	"html/template"
+	"log/slog"
 	"net/http"
 	"strconv"
 	"time"
 )
 
-type controller struct {
-	repo repository
-	tmpl *template.Template
+type response struct {
+	Message string `json:"message"`
 }
 
-func NewController(repo repository, tmpl *template.Template) *controller {
-	return &controller{repo, tmpl}
+func WriteResponseJSON(w http.ResponseWriter, statusCode int, message string) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_ = json.NewEncoder(w).Encode(response{
+		Message: "message",
+	})
+}
+
+type controller struct {
+	repo   repository
+	tmpl   *template.Template
+	logger *slog.Logger
+}
+
+func NewController(repo repository, tmpl *template.Template, logger *slog.Logger) *controller {
+	return &controller{repo, tmpl, logger}
 }
 
 func (c *controller) Create(w http.ResponseWriter, r *http.Request) {
 	err := c.repo.Add(r.Context())
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(err.Error()))
+		c.logger.ErrorContext(r.Context(), "error adding new 💩", "error", err.Error(), "remote_addr", r.RemoteAddr)
+		WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 		return
 	}
+	c.logger.InfoContext(r.Context(), "new 💩 added!", "remote_addr", r.RemoteAddr)
 	w.WriteHeader(http.StatusCreated)
 }
 
 func (c *controller) Delete(w http.ResponseWriter, r *http.Request) {
 	err := c.repo.DeleteLast(r.Context())
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(err.Error()))
+		c.logger.ErrorContext(r.Context(), "error removing last 💩", "error", err.Error(), "remote_addr", r.RemoteAddr)
+		WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 		return
 	}
+	c.logger.InfoContext(r.Context(), "last 💩 removed!", "remote_addr", r.RemoteAddr)
 	w.WriteHeader(http.StatusNoContent)
 }
 
@@ -40,30 +57,30 @@ func (c *controller) GetMonthly(w http.ResponseWriter, r *http.Request) {
 	yearStr := r.PathValue("year")
 	year, err := strconv.ParseUint(yearStr, 10, 64)
 	if err != nil {
-		_ = c.tmpl.ExecuteTemplate(w, "404", nil)
+		c.fourOFour(w, r)
 		return
 	}
 
 	monthlyData, err := c.repo.GetMonthlyByYear(r.Context(), year)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(err.Error()))
+		c.logger.ErrorContext(r.Context(), "error getting monthly 💩", "error", err.Error(), "remote_addr", r.RemoteAddr)
+		WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 		return
 	}
 
 	if len(monthlyData) == 0 {
-		_ = c.tmpl.ExecuteTemplate(w, "404", nil)
+		c.fourOFour(w, r)
 		return
 	}
 
 	lastDataAt, err := c.repo.GetLastDataTimestamp(r.Context())
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(err.Error()))
+		c.logger.ErrorContext(r.Context(), "error getting last 💩", "error", err.Error(), "remote_addr", r.RemoteAddr)
+		WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 		return
 	}
-
-	_ = c.tmpl.ExecuteTemplate(w, "year", struct {
+	w.WriteHeader(http.StatusOK)
+	err = c.tmpl.ExecuteTemplate(w, "year", struct {
 		LastDataAt time.Time
 		Data       []AggData
 		Year       int
@@ -72,38 +89,42 @@ func (c *controller) GetMonthly(w http.ResponseWriter, r *http.Request) {
 		Data:       monthlyData,
 		LastDataAt: lastDataAt,
 	})
+	if err != nil {
+		c.logger.ErrorContext(r.Context(), "error executing year template", "error", err.Error(), "remote_addr", r.RemoteAddr)
+	}
 }
 
 func (c *controller) GetDaily(w http.ResponseWriter, r *http.Request) {
 	yearStr := r.PathValue("year")
 	year, err := strconv.ParseUint(yearStr, 10, 64)
 	if err != nil {
-		_ = c.tmpl.ExecuteTemplate(w, "404", nil)
+		c.fourOFour(w, r)
 		return
 	}
 	monthStr := r.PathValue("month")
 	month, err := strconv.ParseUint(monthStr, 10, 8)
 	if err != nil {
-		_ = c.tmpl.ExecuteTemplate(w, "404", nil)
+		c.fourOFour(w, r)
 		return
 	}
 	dailyData, err := c.repo.GetDailyByMonthAndYear(r.Context(), year, month)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(err.Error()))
+		c.logger.ErrorContext(r.Context(), "error getting daily 💩", "error", err.Error(), "remote_addr", r.RemoteAddr)
+		WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 		return
 	}
 	if len(dailyData) == 0 {
-		_ = c.tmpl.ExecuteTemplate(w, "404", nil)
+		c.fourOFour(w, r)
 		return
 	}
 	lastDataAt, err := c.repo.GetLastDataTimestamp(r.Context())
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(err.Error()))
+		c.logger.ErrorContext(r.Context(), "error getting last 💩", "error", err.Error(), "remote_addr", r.RemoteAddr)
+		WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 		return
 	}
-	_ = c.tmpl.ExecuteTemplate(w, "month", struct {
+	w.WriteHeader(http.StatusOK)
+	err = c.tmpl.ExecuteTemplate(w, "month", struct {
 		LastDataAt time.Time
 		Data       []AggData
 		Year       int
@@ -114,4 +135,15 @@ func (c *controller) GetDaily(w http.ResponseWriter, r *http.Request) {
 		Data:       dailyData,
 		LastDataAt: lastDataAt,
 	})
+	if err != nil {
+		c.logger.ErrorContext(r.Context(), "error executing month template", "error", err.Error(), "remote_addr", r.RemoteAddr)
+	}
+}
+
+func (c *controller) fourOFour(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	err := c.tmpl.ExecuteTemplate(w, "404", nil)
+	if err != nil {
+		c.logger.ErrorContext(r.Context(), "error executing 404 template", "error", err.Error(), "remote_addr", r.RemoteAddr)
+	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"embed"
+	"errors"
 	"fmt"
 	"html/template"
+	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
 	"sync"
 	"time"
@@ -17,6 +21,10 @@ import (
 
 //go:embed templates/*
 var templatesFS embed.FS
+
+var logger = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+	AddSource: true,
+}))
 
 func main() {
 	db, err := db.NewConn(os.Getenv("POSTGRES_URL"))
@@ -51,62 +59,103 @@ func main() {
 			return strings.Repeat("💩", n)
 		},
 	}).ParseFS(templatesFS, "*/*.html"))
-	controller := berak.NewController(repo, tmpl)
+	controller := berak.NewController(repo, tmpl, logger)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
 		http.Redirect(w, r, fmt.Sprintf("/%d", now.Year()), http.StatusTemporaryRedirect)
 	})
-	mux.Handle("POST /berak", rateLimit(protected(controller.Create)))
-	mux.Handle("DELETE /berak", rateLimit(protected(controller.Delete)))
+	mux.HandleFunc("POST /berak", rateLimit(protected(http.HandlerFunc(controller.Create))))
+	mux.HandleFunc("DELETE /berak", rateLimit(protected(http.HandlerFunc(controller.Delete))))
 	mux.HandleFunc("GET /{year}", controller.GetMonthly)
 	mux.HandleFunc("GET /{year}/{month}", controller.GetDaily)
 	mux.HandleFunc("GET /healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte("ok"))
 	})
-	err = http.ListenAndServe(":"+os.Getenv("PORT"), mux)
+	srv := new(http.Server)
+	srv.Handler = logRequest(mux)
+	srv.Addr = fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("http error listening", "error", err.Error())
+		}
+	}()
+	logger.Info(fmt.Sprintf("server listeing at %s", srv.Addr))
+	<-ctx.Done()
+	logger.Info("shutting down server")
+	err = srv.Shutdown(ctx)
 	if err != nil {
-		panic(err)
+		logger.Error("error shutting down server", "error", err.Error())
 	}
 }
 
-func protected(next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+func protected(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("X-API-KEY") != os.Getenv("BERAK_KEY") {
-			w.WriteHeader(http.StatusForbidden)
-			_, _ = w.Write([]byte("gaboleh"))
+		if r.Header.Get("X-Api-Key") != os.Getenv("BERAK_KEY") {
+			logger.WarnContext(r.Context(), "incorrect API-Key", "remote_addr", r.RemoteAddr, "api-key", r.Header.Get("X-Api-Key"))
+			berak.WriteResponseJSON(w, http.StatusUnauthorized, "gaboleh 😡")
 			return
 		}
-		next(w, r)
+		next.ServeHTTP(w, r)
 	}
 }
 
 var users = new(sync.Map)
 
-func rateLimit(next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+func rateLimit(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		apiKey := r.Header.Get("X-API-KEY")
 		if apiKey == "" {
-			w.WriteHeader(http.StatusForbidden)
-			_, _ = w.Write([]byte("gaboleh"))
+			logger.WarnContext(r.Context(), "empty API-Key", "remote_addr", r.RemoteAddr)
+			berak.WriteResponseJSON(w, http.StatusUnauthorized, "gaboleh 😡")
 			return
 		}
 		lastAccess, ok := users.LoadOrStore(apiKey, time.Now())
 		if ok {
 			lastAccessedAt, ok := lastAccess.(time.Time)
 			if !ok {
-				w.WriteHeader(http.StatusInternalServerError)
-				_, _ = w.Write([]byte("error"))
+				logger.ErrorContext(r.Context(), "failed to assert interface to time.Time", "remote_addr", r.RemoteAddr)
+				berak.WriteResponseJSON(w, http.StatusInternalServerError, "it's our fault, not yours!")
 				return
 			}
 			if time.Since(lastAccessedAt) < time.Minute {
-				w.WriteHeader(http.StatusTooManyRequests)
-				_, _ = w.Write([]byte("kecepeten"))
+				logger.WarnContext(r.Context(), "rate limited", "api-key", apiKey, "remote_addr", r.RemoteAddr)
+				berak.WriteResponseJSON(w, http.StatusTooManyRequests, "kecepeten 😡")
 				return
 			}
 			users.Store(apiKey, time.Now())
 		}
-		next(w, r)
+		next.ServeHTTP(w, r)
+	}
+}
+
+type wrapperResponseWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func (wrw *wrapperResponseWriter) WriteHeader(code int) {
+	wrw.code = code
+	wrw.ResponseWriter.WriteHeader(code)
+}
+
+func logRequest(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		wrw := &wrapperResponseWriter{
+			ResponseWriter: w,
+		}
+		t0 := time.Now()
+		next.ServeHTTP(wrw, r)
+		if wrw.code < 400 {
+			logger.InfoContext(r.Context(), fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto), "remote_addr", r.RemoteAddr, "code", wrw.code, "took", time.Since(t0))
+		} else if wrw.code < 500 {
+			logger.WarnContext(r.Context(), fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto), "remote_addr", r.RemoteAddr, "code", wrw.code, "took", time.Since(t0))
+		} else {
+			logger.ErrorContext(r.Context(), fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto), "remote_addr", r.RemoteAddr, "code", wrw.code, "took", time.Since(t0))
+		}
 	}
 }
